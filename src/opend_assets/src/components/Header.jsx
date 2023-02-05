e65352d4008ac6c6ed246fb54ef47aa0008ed88d
Enes Yarali <s302845@studenti.polito.it>
2023-02-06 00:39:37 +0100

    Routes

--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -1,29 +1,51 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import {BrowserRouter , Link , Switch , Route} from "react-router-dom";
+import homeImage from "../../assets/home-img.png";
+import Minter from "./Minter";
+import Gallery from "./Gallery";
 
 function Header() {
   return (
+    <BrowserRouter>
     <div className="app-root-1">
       <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
         <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
           <div className="header-left-4"></div>
           <img className="header-logo-11" src={logo} />
           <div className="header-vertical-9"></div>
+          <Link to="/">
           <h5 className="Typography-root header-logo-text">OpenD</h5>
+          </Link>
           <div className="header-empty-6"></div>
           <div className="header-space-8"></div>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            Discover
+           <Link to="/discover"> Discover</Link>
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            Minter
+            <Link to="/minter">Minter</Link>
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
-            My NFTs
+            <Link to="/collection"> My NFTs </Link>
           </button>
         </div>
       </header>
     </div>
+    <Switch>
+      <Route path="/discover">
+        <h1>Discover</h1>
+      </Route>
+      <Route path="/minter">
+        <Minter/>
+      </Route>
+      <Route path="/collection">
+        <Gallery/>
+      </Route>
+      <Route path="/">
+      <img className="bottom-space" src={homeImage} />
+      </Route>
+    </Switch>
+    </BrowserRouter> 
   );
 }
 
